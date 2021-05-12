1d4c810e4c123bb78c5375fe2c84e26dd7770566
Pavlin Ivanov <79521032+pavlin-js@users.noreply.github.com>
2021-05-12 20:20:35 +0300

    initialize next app

---
+++ .history/pages/_app_20210512201235.js
@@ -0,0 +1,9 @@
+import '../styles/globals.css'
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <L
+  )
+}
+
+export default MyApp
---
+++ .history/pages/_app_20210512201237.js
@@ -0,0 +1,9 @@
+import '../styles/globals.css'
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <Lay
+  )
+}
+
+export default MyApp
